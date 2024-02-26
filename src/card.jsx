be80af8ce507ff6e20fb8c4f8dde3ba433fcaf83
enzoperce35 <enzoperce35@gmail.com>
2024-02-27 07:29:44 +0800

    app working

---
+++ src/card.jsx
@@ -0,0 +1,24 @@
+import { useEffect } from "react"
+import { useState } from "react";
+
+export default function Card({card, select}) {
+  const [cardImage, setCardImage] = useState(null)
+
+  useEffect(() => {
+    setCardImage(`https://static-7.studiobebop.net/ygo_data/card_images/${card.name}.jpg`)
+
+    return (() => {
+      setCardImage(null)
+    })
+  }, [card])
+
+
+  return (
+    <a onClick={(e) => {
+      e.preventDefault()
+      select(card)
+    }}>
+      <img src={cardImage} />
+    </a>
+  )
+}
